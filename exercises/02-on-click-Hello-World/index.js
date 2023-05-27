9e68dba69b67ad136345963c04f98ee52b6c8238
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-05-27 12:31:57 +0000

    First commit

--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,10 @@
 //Declare your function here
+window.clickHelloWorld = function clickHelloWorld() {
+    alert("Hello World");
+    console.log("Hello World in the console");
+
+};
+
+window.myClickFunction = function myClickFunction() {
+	alert("Your first function!");
+};
